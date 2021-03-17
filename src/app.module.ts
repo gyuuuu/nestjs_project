a0b7f0a492b51b25af767ec80ecc2590e4850a5b
gyuuuu <kai9702@gmail.com>
2021-03-18 02:19:47 +0900

    env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,40 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi'; // 타입스크립트나 nest로 되어있지 않은 모듈을 import할 때 사용하는 방법
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 전역적으로 사용 가능
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod', // prod 환경에서는 env 파일을 불러오지 않고 다른 방법으로 환경변수 주입
+      validationSchema: Joi.object({ // config의 유효성 검사
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    RestaurantsModule,
+    GraphQLModule.forRoot({
+    autoSchemaFile: true,
+  }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: true,
+  }),
+],
   controllers: [],
   providers: [],
 })
