2334762b53a7fd398eceb4c862fe1e84b0dd506a
gyuuuu <kai9702@gmail.com>
2021-03-23 01:19:41 +0900

    jwt middleware with app.use

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { jwtMiddleware } from './jwt/jwt.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // 유효성 검증
+  app.use(jwtMiddleware);
   await app.listen(3000);
 }
 bootstrap();
