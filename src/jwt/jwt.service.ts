381d6a88fb3ca4d653ed60f0199e1070dfc6cbd0
gyuuuu <kai9702@gmail.com>
2021-03-23 00:57:15 +0900

    jwt moudule

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+import { CONFIG_OPTIONS } from './jwt.constants';
+import { JwtModuleOptions } from './jwt.interface';
 
 @Injectable()
 export class JwtService {
-  hello() {
-    console.log('hello');
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+  ) {}
+  sign(userid: number): string {
+    return jwt.sign({ id: userid }, this.options.privateKey);
   }
 }
