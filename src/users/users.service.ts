37bfb438c51364b5e504e9a81373a63eea151c46
gyuuuu <kai9702@gmail.com>
2021-03-22 23:24:19 +0900

    jwt module with forRoot

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,12 +6,14 @@ import { createAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { ConfigService } from '@nestjs/config';
+import { JwtService } from 'src/jwt/jwt.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
     private readonly config: ConfigService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async createAccout({
@@ -53,7 +55,7 @@ export class UsersService {
       const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
       return {
         ok: true,
-        token: 'test',
+        token,
       };
     } catch (error) {
       return {
